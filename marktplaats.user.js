e9f7ce412dff7a16b6354bbf4190b6fe46dd2418
Ralph <ralph@Ralphs-MacBook-Air.local>
2019-05-17 18:12:29 +0200

    Add marktplaats tampermonkey script

---
+++ marktplaats.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Marktplaats delete all
+// @namespace    ttps://github.com/ralbuh/tampermonkey
+// @version      1.0
+// @description  Marktplaats add delete all notifications button
+// @author       Ralbuh
+// @match        http*://*marktplaats.nl/notifications
+// @grant        none
+// ==/UserScript==
+
+function deleteAll() {
+	[...document.querySelectorAll('.mp-svg-delete')]
+		.forEach(del => del.click());
+}
+
+(function() {
+    'use strict';
+		
+	var button=document.createElement("input");
+	button.type="button";
+	button.value="Delete all";
+	button.onclick = deleteAll;
+	button.setAttribute("style", "font-size:18px;position:absolute;top:120px;right:40px;");
+	document.body.appendChild(button); 
+	
+})();
