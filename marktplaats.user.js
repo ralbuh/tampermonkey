bc62afcaa6e73d5ab2383f43165a2be24713840b
Ralph <ralph@Ralphs-MacBook-Air.local>
2019-05-17 18:15:14 +0200

    Add marktplaats tampermonkey script

--- marktplaats.user.js
+++ marktplaats.user.js
@@ -4,7 +4,7 @@
 // @version      1.0
 // @description  Marktplaats add delete all notifications button
 // @author       Ralbuh
-// @match        marktplaats.nl/notifications
+// @match        https://www.marktplaats.nl/notifications
 // @grant        none
 // ==/UserScript==
 
