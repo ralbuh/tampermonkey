857e7dc59a99cc2824e617d515ef683906332ae4
rboijmans <rboijmans@bol.com>
2020-11-19 19:30:01 +0100

    Improved flexkids to include not visible/loaded pictures yet

--- flexkids.js
+++ flexkids.js
@@ -8,14 +8,26 @@
 // @grant        GM_download
 // ==/UserScript==
 
-function downloadAll() {
+// there is some loading going on, at first only 10 pictures are shown, with the right button more can be loaded based on some album ids, so now using the other download function
+function downloadAllLoaded() {
 	[...document.querySelectorAll('.fotoalbumimages.images > img')].forEach(img => {
        // console.log(img.src);
-        var filename = img.src.match(/mediajpg\/media\/([0-9]*)/i)[1];
+        //var filename = img.src.match(/mediajpg\/media\/([0-9]*)/i)[1];
+        var filename = img.getAttribute("data-id");
         var downloadUrl = `https://blos.flexkids.nl/ouder/media/download/media/${filename}`;
         var localName = `${filename}.jpg`;
         var arg = { url: downloadUrl, name: localName }
         GM_download(arg);
+    });
+}
+
+function downloadAll() {
+    fotoalbumViewer.albums[0].forEach(id => {
+        var downloadUrl = `https://blos.flexkids.nl/ouder/media/download/media/${id}`;
+        var localName = `${id}.jpg`;
+        var arg = { url: downloadUrl, name: localName }
+        GM_download(arg);
+
     });
 }
 
