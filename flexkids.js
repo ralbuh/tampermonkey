628cc94bd7ccd3b875683f5c582d513e57d0a24e
rboijmans <rboijmans@bol.com>
2020-11-19 15:43:26 +0100

    Add script for flexkids

--- flexkids.js
+++ flexkids.js
@@ -10,11 +10,11 @@
 
 function downloadAll() {
 	[...document.querySelectorAll('.fotoalbumimages.images > img')].forEach(img => {
-//        console.log(img.src);
+       // console.log(img.src);
         var downloadUrl = img.src.replace("formaat/klein","formaat/groot");
         var filename = img.src.match(/mediajpg\/media\/([0-9]*)/i)[1];
-        var localName = `${filename}.jpg`;
-        var arg = { url: downloadUrl, name: localName };
+        // var localName = `${filename}.jpg`;
+        var arg = { url: downloadUrl, name: filename+".jpg" };
         GM_download(arg);
     });
 }
