67bf73a2b484b8e530195cb29f6b594b8c4371fb
ralbuh <ralbuh@gmx.com>
2020-11-20 13:07:30 +0100

    Update flexkids.js

--- flexkids.js
+++ flexkids.js
@@ -1,25 +1,12 @@
 // ==UserScript==
 // @name         flexkids
-// @namespace    https://github.com/ralbuh/tampermonkey
+// @namespace    ttps://github.com/ralbuh/tampermonkey
 // @version      0.1
 // @description  Blos flexkids download all pictures from fotoalbum page
 // @author       Ralbuh
 // @match        https://blos.flexkids.nl/ouder/fotoalbum
 // @grant        GM_download
 // ==/UserScript==
-
-// there is some loading going on, at first only 10 pictures are shown, with the right button more can be loaded based on some album ids, so now using the other download function
-function downloadAllLoaded() {
-	[...document.querySelectorAll('.fotoalbumimages.images > img')].forEach(img => {
-       // console.log(img.src);
-        //var filename = img.src.match(/mediajpg\/media\/([0-9]*)/i)[1];
-        var filename = img.getAttribute("data-id");
-        var downloadUrl = `https://blos.flexkids.nl/ouder/media/download/media/${filename}`;
-        var localName = `${filename}.jpg`;
-        var arg = { url: downloadUrl, name: localName }
-        GM_download(arg);
-    });
-}
 
 function getDateFromMetaData(fotoId, func) {
    		$.post("/ouder/fotoalbum/fotometa", {
