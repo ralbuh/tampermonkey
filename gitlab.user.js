4bb1a75508d6b6a5605463616711f633d7db9481
rboijmans <rboijmans@bol.com>
2023-08-30 08:12:25 +0200

    fix

--- gitlab.user.js
+++ gitlab.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         gitlab merge all merge requests
 // @namespace    https://github.com/ralbuh/tampermonkey
-// @version      1.0.3
+// @version      1.0.4
 // @downloadURL  https://github.com/ralbuh/tampermonkey/raw/master/gitlab.user.js
 // @updateURL    https://github.com/ralbuh/tampermonkey/raw/master/gitlab.user.js
 // @description  Add merge all button for merge request page, will merge everything with gitlab api v4 using csrf-token
 // @author       ralbuh
-// @match        https://*gitlab*/*merge_requests*
+// @include      *gitlab*merge_requests*
 // ==/UserScript==
 
 function mergeAll() {
