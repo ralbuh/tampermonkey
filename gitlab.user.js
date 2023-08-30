b6e1784f25650dc30bece2a82396f3ab212b99d8
rboijmans <rboijmans@bol.com>
2023-08-30 07:31:29 +0200

    add gitlab merge all button

---
+++ gitlab.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         gitlab merge all merge requests
+// @namespace    https://github.com/ralbuh/tampermonkey
+// @version      1.0.0
+// @updateURL    
+// @description  Add merge all button for merge request page, will merge everything with gitlab api v4 using csrf-token
+// @author       ralbuh
+// @include      *gitlab*merge_requests*
+// ==/UserScript==
+
+function mergeAll() {
+    let csrfToken = document.querySelector('meta[name="csrf-token"]').content
+    let baseUrl = window.location.origin + '/'
+
+    document.querySelectorAll('li.merge-request .merge-request-title a').forEach(link => {
+        let splittedUrl = link.href.split('/-/merge_requests/')
+        let projectNamespace = splittedUrl[0].replace(baseUrl, '').trim();
+        let mergeRequestID = splittedUrl[1];
+
+        fetch(`/api/v4/projects/${encodeURIComponent(projectNamespace)}`)
+            .then(res => res.json())
+            .then(project => {
+                console.log(`Merging merge request: [/api/v4/projects/${project.id}/merge_requests/${mergeRequestID}/merge] for project: [${project.name_with_namespace}]`)
+
+                fetch(`/api/v4/projects/${project.id}/merge_requests/${mergeRequestID}/merge`, { method: 'PUT', headers: { 'X-CSRF-TOKEN': csrfToken } })
+                    .then(res => res.json())
+                    .then(mr => {
+                        console.log(`Merged merge request: [/api/v4/projects/${project.id}/merge_requests/${mergeRequestID}/merge] with description: [${mr.description}]`)
+                        console.log(mr)
+                    })
+            })
+    }
+    )
+}
+
+
+(async () => {
+    'use strict';
+
+    const mergeAllButton = `
+        <div class="gl-ml-3">
+            <a class="btn gl-button btn-confirm merge-all-btn" onclick="mergeAll()">Merge All</a>
+        </div>
+    `;
+
+    document.querySelector('.filter-dropdown-container').insertAdjacentHTML("afterend",mergeAllButton)
+})();
