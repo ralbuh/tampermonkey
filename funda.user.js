71e6e0cf3c5dd78682972132a3d97894f22f2139
ralbuh <ralbuh@gmx.com>
2024-05-17 19:57:57 +0200

    Create funda.user.js

---
+++ funda.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         funda
+// @namespace    https://github.com/ralbuh/funda
+// @version      0.1
+// @description  Funda download all pictures of a house
+// @author       Ralbuh
+// @match        https://www.funda.nl/*
+// @grant        GM_download
+// ==/UserScript==
+
+function getHighestSrcFromSrcSet(srcSet) {
+    return srcSet.split(",")
+      .reduce(
+        (acc, item) => {
+          let [url, width] = item.trim().split(" ");
+          width = parseInt(width);
+          if (width > acc.width) return { width, url };
+          return acc;
+        },
+        { width: 0, url: "" }
+      ).url
+}
+
+
+function downloadAll() {
+    [...document.querySelectorAll('.media-viewer-fotos__item img')].forEach(foto => {
+        let downloadUrl = getHighestSrcFromSrcSet(foto.srcset)
+        let fileName = downloadUrl.split('/').pop()
+        console.log(`url: ${downloadUrl}, srcset: ${foto.srcset}`)
+        GM_download({
+            url: downloadUrl,
+            name: fileName
+        });
+    });
+}
+
+
+(function() {
+    'use strict';
+    var button = document.createElement("input");
+    button.type = "button";
+    button.value = "Download all";
+    button.onclick = downloadAll;
+    button.setAttribute("style", "font-size:18px;position:absolute;bottom:0px;right:0px;");
+    document.body.appendChild(button);
+})();
